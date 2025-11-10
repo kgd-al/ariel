8df2f5ab10c28df316af60feaf62fe4d1f17a4e3
Kevin Godin-Dubois <k.j.m.godin-dubois@vu.nl>
2025-11-10 07:44:37 +0100

    .

--- src/ariel/utils/renderers.py
+++ src/ariel/utils/renderers.py
@@ -13,6 +13,7 @@ from pathlib import Path
 # Third-party libraries
 import numpy as np
 import mujoco
+import numpy as np
 from PIL import Image
 
 # Local libraries
@@ -90,11 +91,14 @@ def single_frame_renderer(
         # Generate frame using rendering engine
         frame = renderer.render()
 
+        if transparent:
+            frame = np.dstack((
+                frame,
+                np.uint8((np.sum(frame, axis=-1) > 0) * 255)
+            ))
+
         # Convert frame into an image which can be shown
         img: Image.Image = Image.fromarray(frame)
-
-    # if transparent:
-    #     img = img.
 
     # Save image locally
     if save:
