24c3f4109ae2b57136237a230892b3c22fc87797
Kevin Godin-Dubois <k.j.m.godin-dubois@vu.nl>
2026-09-24 07:46:28 +0200

    .

--- src/ariel/simulation/environments/_base_world.py
+++ src/ariel/simulation/environments/_base_world.py
@@ -215,7 +215,7 @@ class BaseWorld:
 
             # AABB
             local_size = model.geom_aabb[geom.id][3:]
-            rotated_size = r_mat @ local_size
+            rotated_size = np.abs(r_mat) @ local_size
             corners = np.array([
                 pos + np.array([dx, dy, dz]) * rotated_size
                 for dx in [-1, 1] for dy in [-1, 1] for dz in [-1, 1]
